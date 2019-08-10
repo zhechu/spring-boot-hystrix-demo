4eb0ce9490397880b2d31b733770fdab21d0896e
zhechu <ling-yu-wang@qq.com>
2019-08-10 11:42:07 +0800

    add: Feign demo

---
+++ spring-boot-hystrix-feign-consumer/src/main/java/com/wise/FeignConsumerApplication.java
@@ -0,0 +1,18 @@
+package com.wise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+/**
+ * 启动类
+ */
+@EnableFeignClients
+@SpringBootApplication
+public class FeignConsumerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FeignConsumerApplication.class, args);
+    }
+
+}
