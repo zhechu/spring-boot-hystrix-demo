8a1790fad64ad34bf4d2428e474df9df5f3b026b
zhechu <ling-yu-wang@qq.com>
2019-08-10 11:40:22 +0800

    add: Feign demo

--- spring-boot-hystrix-general-consumer/src/main/java/com/wise/controller/UserController.java
+++ spring-boot-hystrix-general-consumer/src/main/java/com/wise/controller/UserController.java
@@ -1,6 +1,8 @@
 package com.wise.controller;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import com.wise.entity.User;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,6 +10,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+
+import java.math.BigDecimal;
 
 /**
  * 用户控制器
@@ -23,10 +27,22 @@ public class UserController {
     private String springBootHystrixProviderName;
 
     @GetMapping("/{id}")
+    // 熔断降级
+    @HystrixCommand(fallbackMethod = "findByIdFallback")
     public User findById(@PathVariable Long id) {
         String url = String.format("http://%s/users/{id}", springBootHystrixProviderName);
         User user = this.restTemplate.getForObject(url, User.class, id);
         return user;
+    }
+
+    /**
+     * 熔断降级回调方法
+     * @param id
+     * @return
+     */
+    public User findByIdFallback(Long id, Throwable throwable) {
+        throwable.printStackTrace();
+        return new User(id, "默认用户", "默认用户", 0, new BigDecimal(1));
     }
 
 }
